236e1872c7b0db142e4975c87d7d302abe81455f
pauldendulk <pauldendulk@gmail.com>
2023-01-03 09:10:18 +0100

    Setup Mapsui project structure without data yet

---
+++ MapsuiGeoparquet/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Mapsui.Layers;
+using Mapsui.Styles;
+using MapsuiGeoparquet.Providers;
+
+namespace MapsuiGeoparquet;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        var mapControl = new Mapsui.UI.Maui.MapControl();
+        mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+        mapControl.Map?.Layers.Add(CreateGeoParquetLayer());
+
+        Content = mapControl;
+    }
+
+    private ILayer CreateGeoParquetLayer()
+    {
+        var geoParquetProvider = new GeoParquetProvider();
+        geoParquetProvider.LoadData();
+
+        return new Layer
+        {
+            Style = new VectorStyle(),
+            DataSource = geoParquetProvider
+        };
+    }
+}
---
+++ MapsuiGeoparquet/Providers/GeoParquetProvider.cs
@@ -0,0 +1,26 @@
+﻿using Mapsui;
+using Mapsui.Layers;
+using Mapsui.Providers;
+
+namespace MapsuiGeoparquet.Providers
+{
+    internal class GeoParquetProvider : IProvider
+    {
+        public string CRS { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public MRect? GetExtent()
+        {
+            return null;
+        }
+
+        public Task<IEnumerable<IFeature>> GetFeaturesAsync(FetchInfo fetchInfo)
+        {
+            return Task.FromResult(Enumerable.Empty<IFeature>());
+        }
+
+        public void LoadData()
+        { 
+        
+        }
+    }
+}
